0cbadc856b7acb49035316db10b423fb45975d91
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2024-01-01 15:40:50 -0600

    Install VitePress

---
+++ .vitepress/config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Zachary K. Watkins",
+  description: "A VitePress Site",
+})

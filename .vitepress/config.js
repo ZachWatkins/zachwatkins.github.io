7c34575d0564432dca92b68467feb201beea117e
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2024-01-01 16:02:21 -0600

    Clear up repository of unnecessary features

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Zachary K. Watkins",
-  description: "A VitePress Site",
+  title: "My Awesome Project",
+  description: "A VitePress Site"
 })

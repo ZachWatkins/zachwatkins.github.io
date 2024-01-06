9b235372d81fd0a64bc4ab2fe5ca15b6757236f8
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2024-01-05 22:02:22 -0600

    Unify theme files

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -1,19 +1,27 @@
 <script setup>
 import { useData } from 'vitepress'
-import Navigation from './Navigation.vue'
 import Home from './Home.vue'
-import Footer from './Footer.vue'
 // https://vitepress.dev/reference/runtime-api#usedata
 const { page, site, theme, frontmatter } = useData()
 </script>
 
 <template>
   <div>
-    <header>
+    <header class="bg-js-yellow text-black">
       <h1>{{ site.title }}</h1>
       <p>{{ site.description }}</p>
     </header>
-    <Navigation />
+    <nav>
+      <span v-for="(link, i) in theme.nav">
+        <span v-if="i > 0"> | </span>
+        <a
+          :key="link.text"
+          :href="link.link"
+        >
+          {{ link.text }}
+        </a>
+      </span>
+    </nav>
     <main v-if="page.isNotFound"> Oh no! </main>
     <main v-else-if="frontmatter.layout === 'home'">
       <Home
@@ -25,10 +33,15 @@ const { page, site, theme, frontmatter } = useData()
       <Content />
     </main>
     <hr />
-    <Footer
-      :copyrightYears="theme.date.copyrightYears"
-      :author="theme.author"
-      :nav="theme.footerNav"
-    />
+    <footer>
+      &copy; {{ theme.date.copyrightYears }} {{ theme.author }}<br />
+      <a
+        v-for="link in theme.footerNav"
+        :key="link.text"
+        :href="link.link"
+      >
+        {{ link.text }}
+      </a>
+    </footer>
   </div>
 </template>

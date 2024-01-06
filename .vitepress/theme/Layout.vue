6214c2f47c5d3e292368ee198b68762b996d4a8e
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2024-01-05 22:54:51 -0600

    Improve header style

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -6,42 +6,44 @@ const { page, site, theme, frontmatter } = useData()
 </script>
 
 <template>
-  <div>
-    <header class="bg-js-yellow text-black">
-      <h1>{{ site.title }}</h1>
-      <p>{{ site.description }}</p>
-    </header>
-    <nav>
-      <span v-for="(link, i) in theme.nav">
-        <span v-if="i > 0"> | </span>
-        <a
-          :key="link.text"
-          :href="link.link"
-        >
-          {{ link.text }}
-        </a>
-      </span>
-    </nav>
-    <main v-if="page.isNotFound"> Oh no! </main>
-    <main v-else-if="frontmatter.layout === 'home'">
-      <Home
-        :hero="frontmatter.hero"
-        :features="frontmatter.features"
-      />
-    </main>
-    <main v-else>
-      <Content />
-    </main>
-    <hr />
-    <footer>
-      &copy; {{ theme.date.copyrightYears }} {{ theme.author }}<br />
+  <header>
+    <div class="max-w-screen-xl m-auto">
+      <div class="font-bold text-4xl">
+        {{ site.title }}
+      </div>
+      <span class="text-xl">{{ site.description }}</span>
+    </div>
+  </header>
+  <nav>
+    <div class="max-w-screen-xl m-auto text-xl">
       <a
-        v-for="link in theme.footerNav"
+        v-for="(link, i) in theme.nav"
         :key="link.text"
         :href="link.link"
       >
         {{ link.text }}
       </a>
-    </footer>
-  </div>
+    </div>
+  </nav>
+  <main v-if="page.isNotFound"> Oh no! </main>
+  <main v-else-if="frontmatter.layout === 'home'">
+    <Home
+      :hero="frontmatter.hero"
+      :features="frontmatter.features"
+    />
+  </main>
+  <main v-else>
+    <Content />
+  </main>
+  <hr />
+  <footer>
+    &copy; {{ theme.date.copyrightYears }} {{ theme.author }}<br />
+    <a
+      v-for="link in theme.footerNav"
+      :key="link.text"
+      :href="link.link"
+    >
+      {{ link.text }}
+    </a>
+  </footer>
 </template>

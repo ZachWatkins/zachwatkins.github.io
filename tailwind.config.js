7e95ee2c768b92500b993dea7d49970e5c83b81b
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2024-01-05 21:57:55 -0600

    Add support for tailwindcss to vitepress theme

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './.vitepress/**/*.js',
+    './.vitepress/**/*.vue',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}

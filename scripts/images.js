1c44661ca66eac4da08db30552c11d3dd750a192
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2024-01-13 12:14:53 -0600

    Fixes

--- scripts/images.js
+++ scripts/images.js
@@ -4,7 +4,6 @@ import fs from 'fs'
 // Constrain the image dimensions at public/me.jp to no more than 2048px wide and tall.
 const profile = sharp('public/pic.jpg')
 const profileFileSize = fs.statSync('public/pic.jpg').size
-const profileFileSizeInKb = profileFileSize / 1024
 profile.metadata().then((metadata) => {
   console.log('before', {
     fileSize: (profileFileSize / 1024).toFixed(2) + ' kb',
@@ -13,6 +12,7 @@ profile.metadata().then((metadata) => {
     density: metadata.density,
     resolutionUnit: metadata.resolutionUnit,
   })
+
   profile
     .resize(2048, 2048, { fit: 'inside' })
     .withMetadata({

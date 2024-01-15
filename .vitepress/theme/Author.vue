b70f7a6502811c9b503aeddd212d879881e4baa8
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2024-01-15 10:26:56 -0600

    Improve author block styling

--- .vitepress/theme/Author.vue
+++ .vitepress/theme/Author.vue
@@ -6,7 +6,7 @@ const { frontmatter, theme } = useData()
 
 <template>
   <dl class="author border-t-2 border-b-2 pt-4 pb-4 mt-6 mb-6">
-    <dt class="text-center mb-2">About the Author</dt>
+    <dt class="text-center mb-2 font-bold text-3xl">About the Author</dt>
     <dd>
       <ul>
         <li class="flex items-center">
@@ -22,21 +22,26 @@ const { frontmatter, theme } = useData()
           </div>
           <dl>
             <dt class="sr-only">Name</dt>
-            <dd class="sr-only">{{ frontmatter.author }}</dd>
+            <dd class="font-bold text-3xl">{{ theme.author }}</dd>
             <dt class="sr-only">Description</dt>
-            <dd class="description"
-              >Zachary Watkins is the lead web application developer for the
-              Communications division at Texas A&M Transportation Institute in
-              College Station, Texas. He spends most of his time creating
-              web-based tools, visualizing data, and writing technical
-              documents. He also posts on Twitter
+            <dd class="text-xl font-family-prose"
+              ><a
+                target="_blank"
+                rel="noopnener noreferrer"
+                href="{{ theme.linkedin }}"
+                >Zachary Watkins</a
+              >
+              is the lead application developer for Communications at Texas A&M
+              Transportation Institute in College Station, Texas. He spends most
+              of his time creating web-based tools, visualizing data, and
+              writing technical documents. He also posts on Twitter
               <a
                 :href="'https://twitter.com/' + theme.twitter"
                 target="_blank"
                 rel="noopnener noreferrer"
                 >{{ theme.twitter }}</a
               >
-              and has a web app for
+              and made a site for
               <a
                 href="https://ohmyword.net"
                 target="_blank"

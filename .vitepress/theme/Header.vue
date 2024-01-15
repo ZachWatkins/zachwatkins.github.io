c6da871c820299ab9dc969d7ddcb560dce62fb33
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2024-01-15 17:41:39 -0600

    Add Header component

---
+++ .vitepress/theme/Header.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { useData } from 'vitepress'
+const { site, theme } = useData()
+</script>
+
+<template>
+  <header class="p-1">
+    <div class="max-w-screen-xl m-auto flex items-center">
+      <div class="p-1 m-4 mt-0 mb-0 font-bold text-4xl flex-grow">
+        <a href="/">{{ site.title }}</a>
+      </div>
+      <nav class="text-xl flex-initial text-right font-bold flex h-full">
+        <a
+          v-for="(link, i) in theme.nav"
+          :key="link.text"
+          :href="link.link"
+          class="p-1 m-4"
+        >
+          {{ link.text }}
+        </a>
+      </nav>
+    </div>
+  </header>
+</template>
--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -1,30 +1,15 @@
 <script setup>
 import { useData } from 'vitepress'
 // https://vitepress.dev/reference/runtime-api#usedata
-const { page, site, theme, frontmatter } = useData()
+const { page, theme, frontmatter } = useData()
+import Header from './Header.vue'
 import Home from './Home.vue'
 import Post from './Post.vue'
 import Articles from './Articles.vue'
 </script>
 
 <template>
-  <header class="p-1">
-    <div class="max-w-screen-xl m-auto flex items-center">
-      <div class="p-1 m-4 mt-0 mb-0 font-bold text-4xl flex-grow">
-        <a href="/">{{ site.title }}</a>
-      </div>
-      <nav class="text-xl flex-initial text-right font-bold flex h-full">
-        <a
-          v-for="(link, i) in theme.nav"
-          :key="link.text"
-          :href="link.link"
-          class="p-1 m-4"
-        >
-          {{ link.text }}
-        </a>
-      </nav>
-    </div>
-  </header>
+  <Header />
   <main
     v-if="'home' === frontmatter.layout"
     class="home pl-5 pr-5 max-w-screen-xl text-xl text-center m-auto"

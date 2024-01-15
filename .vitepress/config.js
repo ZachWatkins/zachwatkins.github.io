b295257768bce57e278462521eb0ce6232824526
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2024-01-14 21:59:51 -0600

    Update twitter handle

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -29,7 +29,7 @@ export default defineConfig({
         href: 'https://fonts.googleapis.com/css2?family=Source+Serif+4:ital,opsz,wght@0,8..60,400;0,8..60,600;0,8..60,900;1,8..60,400;1,8..60,600;1,8..60,900&display=swap',
       },
     ],
-    ['meta', { name: 'twitter:site', content: '@zachtypedit' }],
+    ['meta', { name: 'twitter:site', content: '@zachwatkins0' }],
     ['meta', { name: 'twitter:card', content: 'summary' }],
     [
       'meta',
@@ -46,7 +46,7 @@ export default defineConfig({
       'Zachary Watkins is the lead web application developer for the Communications division at Texas A&M Transportation Institute in College Station, Texas. He spends most of his time working closely with researchers to create web-based tools, visualize data, and scope technical work for his team. He also has many side projects, including one for crossword puzzle enthusiasts.',
     gravatar:
       '049719b54832b7e452d5ba7f791da76c3eb1952cf234e58f25eea074d5976212',
-    twitter: '@zachtypedit',
+    twitter: '@zachwatkins0',
     date: {
       copyrightYears:
         2024 === new Date().getFullYear()
@@ -78,7 +78,7 @@ export default defineConfig({
     ],
     socialLinks: [
       { icon: 'github', link: 'https://github.com/zachwatkins/' },
-      { icon: 'x', link: 'https://x.com/zachtypedit' },
+      { icon: 'x', link: 'https://x.com/zachwatkins0' },
       {
         icon: 'linkedin',
         link: 'https://www.linkedin.com/in/zacharykwatkins/',

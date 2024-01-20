1fb7e850231c821f8c4be4b22e44d9f649a638b9
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2024-01-19 21:48:55 -0600

    Improve responsiveness of hedaer

--- .vitepress/theme/Header.vue
+++ .vitepress/theme/Header.vue
@@ -1,29 +1,31 @@
 <script setup>
 import { useData } from 'vitepress'
-import Search from './Search.vue'
+import VPNavBarSearch from '../../node_modules/vitepress/dist/client/theme-default/components/VPNavBarSearch.vue'
 const { site, theme } = useData()
 </script>
 
 <template>
-  <header class="p-1">
-    <div class="max-w-screen-xl m-auto flex items-center">
-      <div class="p-1 m-4 mt-0 mb-0 font-bold text-4xl flex-grow">
+  <header class="p-4 text-center md:text-left">
+    <div class="w-full max-w-screen-lg m-auto md:flex md:items-center">
+      <div class="p-1 font-bold text-2xl lg:text-4xl md:flex-grow">
         <a href="/">{{ site.title }}</a>
       </div>
       <div
-        class="flex-initial h-full flex p-1 m-4 mt-0 mb-0 text-right text-xl"
+        class="w-full md:w-auto md:flex-initial md:h-full md:flex p-1 md:m-4 mt-0 mb-0 md:text-right text-xl md:items-center"
       >
         <nav class="font-bold">
           <a
             v-for="(link, i) in theme.nav"
             :key="link.text"
             :href="link.link"
-            class="p-1 m-4"
+            class="p-1 w-1/4 inline-block md:w-auto md:m-2 lg:m-4"
           >
             {{ link.text }}
           </a>
         </nav>
-        <Search />
+        <div id="search">
+          <VPNavBarSearch class="search pl-0" />
+        </div>
       </div>
     </div>
   </header>
--- .vitepress/theme/Search.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import VPNavBarSearch from '../../node_modules/vitepress/dist/client/theme-default/components/VPNavBarSearch.vue'
-</script>
-
-<template>
-  <div id="search">
-    <VPNavBarSearch class="search" />
-  </div>
-</template>

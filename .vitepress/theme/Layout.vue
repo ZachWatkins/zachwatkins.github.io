ca28397c978f5edd9bd17d8af2f2273f89675bd3
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2024-01-20 22:20:30 -0600

    Improve responsive home page layout

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -12,13 +12,13 @@ import Articles from './Articles.vue'
   <Header />
   <main
     v-if="'index.md' === page.filePath"
-    class="home pl-5 pr-5 max-w-screen-xl text-xl text-center m-auto"
+    class="home flex-grow flex flex-col justify-center pl-5 pr-5 max-w-screen-xl text-xl text-center ml-auto mr-auto"
   >
     <Home />
   </main>
   <main
     v-else
-    class="pl-10 pr-5 w-full m-auto"
+    class="pl-10 pr-5 pt-5 w-full m-auto"
   >
     <Articles v-if="'articles/index.md' === page.filePath" />
     <Articles
@@ -35,19 +35,19 @@ import Articles from './Articles.vue'
   </main>
   <footer class="p-1">
     <div
-      class="text-center max-w-screen-lg m-auto md:text-left md:flex md:items-center md:h-full"
+      class="text-center max-w-screen-lg m-auto md:text-left"
     >
-      <div class="text-lg md:text-xl md:flex-grow">
+      <div class="text-lg md:text-xl">
         <a
           v-for="link in theme.footerNav"
           :key="link.text"
           :href="link.link"
-          class="ml-2 mr-2 inline-block md:m-2 lg:m-4 whitespace-nowrap"
+          class="ml-2 mr-2 mt-1 mb-1 inline-block whitespace-nowrap text-lg md:ml-4 md:mr-4 md:mt-2 md:mb-2"
         >
           {{ link.text }}
         </a>
       </div>
-      <div class="flex-initial p-1 m-4 mt-0 mb-0 md:text-right">
+      <div class="p-1 m-4 mt-0 mb-0 md:text-right">
         v{{ theme.version }} &copy; {{ theme.date.copyrightYears }}
         {{ theme.author }}. All rights reserved.
       </div>

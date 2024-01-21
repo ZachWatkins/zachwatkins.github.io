fee5e06f17c930b9425de17d26b64b76ce34d639
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2024-01-20 22:41:08 -0600

    Improve home page layout

--- .vitepress/theme/Home.vue
+++ .vitepress/theme/Home.vue
@@ -4,11 +4,11 @@ const { frontmatter } = useData()
 </script>
 
 <template>
-  <h1 class="text-6xl mb-8 leading-tight"
-    ><span class="font-extrabold">{{ frontmatter.hero.name }}<br /></span
-    ><span class="">{{ frontmatter.hero.text }}</span></h1
+  <h1 class="leading-10 md:text-6xl mb-8 md:leading-tight"
+    ><span class="font-extrabold">{{ frontmatter.hero.name }}<br class="hidden md:block" /></span
+    ><span class="ml-2">{{ frontmatter.hero.text }}</span></h1
   >
-  <p class="text-2xl w-3/4 mb-8 ml-auto mr-auto">
+  <p class="text-2xl md:w-3/4 mb-8 ml-auto mr-auto">
     {{ frontmatter.hero.tagline }}
   </p>
   <p class="mb-8">

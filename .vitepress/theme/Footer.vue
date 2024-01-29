b6fc2132c3a5d9dfb39f09b84204843e31e95469
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2024-01-28 22:34:31 -0600

    Extract footer component and improve main spacing

---
+++ .vitepress/theme/Footer.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { useData } from 'vitepress'
+const { theme } = useData()
+</script>
+
+<template>
+  <footer
+    class="w-full text-center max-w-screen-lg m-auto md:text-left p-4 pt-0 mt-4 md:pb-8 md:mt-8"
+  >
+    <div class="text-lg md:text-xl">
+      <a
+        v-for="link in theme.footerNav"
+        :key="link.text"
+        :href="link.link"
+        class="mr-5 mb-2 inline-block whitespace-nowrap text-lg"
+      >
+        {{ link.text }}
+      </a>
+    </div>
+    <div class="md:text-right">
+      v{{ theme.version }} &copy; {{ theme.date.copyrightYears }}
+      {{ theme.author }}. All rights reserved.
+    </div>
+  </footer>
+</template>
--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -3,6 +3,7 @@ import { useData } from 'vitepress'
 // https://vitepress.dev/reference/runtime-api#usedata
 const { page, site, theme, frontmatter, params } = useData()
 import Header from './Header.vue'
+import Footer from './Footer.vue'
 import Home from './Home.vue'
 import Article from './Article.vue'
 import Articles from './Articles.vue'
@@ -15,7 +16,7 @@ import Articles from './Articles.vue'
     class="home flex-grow flex flex-col justify-center pl-5 pr-5 max-w-screen-xl text-xl text-center ml-auto mr-auto"
   >
   </main>
-  <main class="pl-10 pr-5 pt-5 w-full m-auto flex-grow">
+  <main class="pl-5 pr-5 w-full m-auto flex-grow">
     <Home v-if="'index.md' === page.filePath" />
     <Articles v-if="'articles/index.md' === page.filePath" />
     <Articles
@@ -30,20 +31,5 @@ import Articles from './Articles.vue'
     <Content v-else-if="!page.isNotFound" />
     <div v-else> Oh no! </div>
   </main>
-  <footer class="w-full p-4 text-center max-w-screen-lg m-auto md:text-left">
-    <div class="text-lg md:text-xl">
-      <a
-        v-for="link in theme.footerNav"
-        :key="link.text"
-        :href="link.link"
-        class="mr-5 mb-2 inline-block whitespace-nowrap text-lg"
-      >
-        {{ link.text }}
-      </a>
-    </div>
-    <div class="md:text-right">
-      v{{ theme.version }} &copy; {{ theme.date.copyrightYears }}
-      {{ theme.author }}. All rights reserved.
-    </div>
-  </footer>
+  <Footer />
 </template>

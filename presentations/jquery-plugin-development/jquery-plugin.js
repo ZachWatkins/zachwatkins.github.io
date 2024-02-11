577a96d78dc762e324168ee9eb88a2ff1a33a350
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2024-02-10 21:55:05 -0600

    Remove unneeded wrapper from plugin

--- presentations/jquery-plugin-development/jquery-plugin.js
+++ presentations/jquery-plugin-development/jquery-plugin.js
@@ -1,29 +1,27 @@
-;(function () {
-  $.fn.gumboot = function (options) {
-    var settings = $.extend(
-      {
-        selector: '.data',
-        separator: ':',
-      },
-      $.fn.gumboot.defaults,
-      options,
-    );
+$.fn.gumboot = function (options) {
+  var settings = $.extend(
+    {
+      selector: '.data',
+      separator: ':',
+    },
+    $.fn.gumboot.defaults,
+    options,
+  );
 
-    // Execute and return for chaining
-    return this.each(function (index) {
-      var data = {},
-        $item = $(this);
+  // Execute and return for chaining
+  return this.each(function (index) {
+    var data = {},
+      $item = $(this);
 
-      $item.find(settings.selector).each(function () {
-        var pair = this.innerHTML.trim().split(settings.separator);
-        data[pair[0].trim()] = pair[1].trim();
-      });
-
-      $item.data('gumboot', data);
-
-      if ($.isFunction(settings.callback)) {
-        settings.callback.call(this, settings, index, length);
-      }
+    $item.find(settings.selector).each(function () {
+      var pair = this.innerHTML.trim().split(settings.separator);
+      data[pair[0].trim()] = pair[1].trim();
     });
-  };
-})();
+
+    $item.data('gumboot', data);
+
+    if ($.isFunction(settings.callback)) {
+      settings.callback.call(this, settings, index, length);
+    }
+  });
+};

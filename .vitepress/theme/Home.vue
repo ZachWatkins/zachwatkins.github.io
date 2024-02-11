71bb46500accd1d686ab046f5c1a4a6c09121bb8
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2024-02-10 22:27:24 -0600

    Add latest article and presentation to the home page

--- .vitepress/theme/Home.vue
+++ .vitepress/theme/Home.vue
@@ -1,6 +1,9 @@
 <script setup>
 import { useData } from 'vitepress';
 import { data as articles } from './articles.data.js';
+import { data as presentations } from './presentations.data.js';
+import ArticleListing from './ArticleListing.vue';
+import PresentationListing from './PresentationListing.vue';
 const { frontmatter } = useData();
 </script>
 
@@ -11,7 +14,7 @@ const { frontmatter } = useData();
     ><span class="ml-2">{{ frontmatter.hero.subtitle }}</span></h1
   >
   <div
-    class="pl-2 pr-2 md:pl-0 md:pr-0 md:text-left font-family-heading text-2xl"
+    class="pl-2 pr-2 md:pl-0 md:pr-0 md:text-left font-family-heading text-2xl vp-doc"
   >
     <a
       class="mb-8 mr-8 md:ml-0 md:mr-16 overflow-clip float-left flow-circle max-w-2/5 md:max-w-none"
@@ -26,18 +29,9 @@ const { frontmatter } = useData();
       {{ frontmatter.hero.tagline }}
     </p>
     <p>
-      Check out my <a href="/articles/">articles</a> on JavaScript, PHP,
+      Sometimes I write <a href="/articles/">articles</a> on JavaScript, PHP,
       Laravel, WordPress, DevOps, and
       <a href="/articles/tags/"> other subjects</a>.
-    </p>
-    <p v-if="articles.length > 0"
-      >I published
-      <a :href="articles[0].url">{{ articles[0].title }}</a>
-      on
-      <time :datetime="articles[0].date.time">{{
-        articles[0].date.string
-      }}</time
-      >.
     </p>
     <p>
       I'm also building a crossword puzzle tool at
@@ -47,6 +41,48 @@ const { frontmatter } = useData();
         >ohmyword.net</a
       >.
     </p>
+    <p
+      >You can also find me on
+      <a
+        target="_blank"
+        href="https://github.com/zachwatkins"
+        >GitHub</a
+      >,
+      <a
+        target="_blank"
+        href="https://codepen.io/zw"
+        >CodePen</a
+      >,
+      <a
+        target="_blank"
+        href="https://profiles.wordpress.org/zachwtx/"
+        >WordPress</a
+      >,
+      <a
+        target="_blank"
+        href="https://www.linkedin.com/in/zacharykwatkins/"
+        >LinkedIn</a
+      >,
+      <a
+        target="_blank"
+        href="https://x.com/zachwatkinsv1"
+        >Twitter</a
+      >,
+      <a
+        target="_blank"
+        href="https://packagist.org/users/zachwatkins/packages/"
+        >Packagist</a
+      >, or via email at
+      <a href="mailto:zach@zachwatkins.dev">zach&#64;zachwatkins.dev</a>.
+    </p>
+    <ArticleListing
+      v-if="articles.length > 0"
+      :article="articles[0]"
+    />
+    <PresentationListing
+      v-if="presentations.length > 0"
+      :presentation="presentations[0]"
+    />
   </div>
   <Content />
 </template>

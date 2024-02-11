16d5110324154c89684bfea96e87c2bad1638dc6
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2024-02-10 22:26:43 -0600

    Create listing version of page types

---
+++ .vitepress/theme/ArticleListing.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+  article: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <article class="vp-doc">
+    <div>
+      <h2 class="mb-0">
+        Latest Article: <a :href="article.url">{{ article.title }}</a>
+      </h2>
+      <dl class="float-left mr-2">
+        <dt class="sr-only">Published on</dt>
+        <dd>
+          <time :datetime="new Date(article.date.time).toISOString()">{{
+            article.date.string
+          }}</time>
+        </dd>
+      </dl>
+      <span class="float-left mr-2">&mdash;</span>
+      <div
+        v-if="article.excerpt"
+        v-html="article.excerpt"
+      ></div>
+    </div>
+    <div>
+      <a
+        aria-label="read more"
+        :href="article.url"
+        >Read more →</a
+      >
+    </div>
+  </article>
+</template>
---
+++ .vitepress/theme/PresentationListing.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+  presentation: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <article class="vp-doc">
+    <div>
+      <h2 class="mb-0">
+        Latest Presentation:
+        <a :href="presentation.url">{{ presentation.title }}</a>
+      </h2>
+      <dl class="float-left mr-2">
+        <dt class="sr-only">Published on</dt>
+        <dd>
+          <time :datetime="new Date(presentation.date.time).toISOString()">{{
+            presentation.date.string
+          }}</time>
+        </dd>
+      </dl>
+      <span class="float-left mr-2">&mdash;</span>
+      <div
+        v-if="presentation.excerpt"
+        v-html="presentation.excerpt"
+      ></div>
+    </div>
+    <div>
+      <a
+        aria-label="read more"
+        :href="presentation.url"
+        >Read more →</a
+      >
+    </div>
+  </article>
+</template>

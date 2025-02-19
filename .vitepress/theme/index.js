c8937d295381f26ebf1b1e0cdd1c5666d2048379
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2025-02-18 19:55:37 -0600

    Fix styles

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,8 +1,8 @@
 // https://vitepress.dev/guide/custom-theme
 import DefaultTheme from 'vitepress/theme-without-fonts'
 import Layout from './Layout.vue';
-import './tailwind.postcss';
 import './style.css';
+import './tailwind.postcss';
 
 /** @type {import('vitepress').Theme} */
 export default {
